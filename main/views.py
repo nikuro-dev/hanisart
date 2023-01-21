3b7f33372b489f5ffbc7985ea198b6658894c559
amirhossein <nikuro-dev>
2023-01-22 01:17:21 +0330

    add a route for main app and create views for main app pages

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+
+def index(request):
+    return HttpResponse("hello world this is first page")
+
+def contact_page(request):
+    return HttpResponse("this is contact us page")
+
+def about_us(request):
+    return HttpResponse("this is about us page")
