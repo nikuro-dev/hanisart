70846579b3f617b4484da3c01896f0588b1af44c
amirhossein <nikuro-dev>
2023-01-23 03:08:02 +0330

    create comment admin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post, Comment
 # Register your models here.
+@admin.register(Comment)
+class NameAdmin(admin.ModelAdmin):
+    list_display = ('name','body', 'post', 'active', 'created_on')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
