a31b3678cd1ecfb9a77c690c7f4aceeb9666229b
amirhossein <nikuro-dev>
2023-01-22 15:38:59 +0330

    create the post model for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post (models.Model):
+
+    class Status (models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB' , 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(default= datetime.now)
+    publish = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length= 2, choices= Status.choices, default= Status.DRAFT)
+
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
