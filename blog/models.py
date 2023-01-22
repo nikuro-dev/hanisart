5a384077b9bab72389415a4175894ce15b3aa395
amirhossein <nikuro-dev>
2023-01-22 17:39:39 +0330

    add many to one relationship from post model to User

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ class Post (models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    author = models.ForeignKey(User, on_delete= models.CASCADE, related_name='blog_posts')
     created = models.DateTimeField(default= datetime.now)
     publish = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
